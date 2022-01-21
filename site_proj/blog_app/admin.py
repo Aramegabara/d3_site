58c35116615230a9bbaddee5d753f406ce658949
aramegabara <geraskov94@gmail.com>
2022-01-21 22:00:30 +0100

    add comments form

--- site_proj/blog_app/admin.py
+++ site_proj/blog_app/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
 #? zwykly sposob registracji
 # admin.site.register(Post)
@@ -14,3 +14,9 @@ class PostAdmin(admin.ModelAdmin):
     raw_id_fields = ('author',) #? dodaje widzet dla tego pola ? ulatwia wyszukiwanie w ADD POST
     date_hierarchy = 'publish' #? dodaje date nad tablica
     ordering = ('status', 'publish') #? sortuje 'default'
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('post', 'name', 'email', 'created', 'active')
+    list_filter = ('active', 'created', 'update')
+    search_fields = ('name', 'email', 'body')
--- site_proj/blog_app/forms.py
+++ site_proj/blog_app/forms.py
@@ -1,8 +1,21 @@
 from django import forms
+from django.forms import ModelForm
+from .models import Comment
 
-
-class EmailPostForm(forms.Form):
+#! form
+class EmailPostForm(forms.Form): 
     name = forms.CharField(max_length=25)
     email = forms.EmailField()
     to = forms.EmailField()
     comments = forms.CharField(required=False, widget=forms.Textarea) #? opcjonalne pole
+
+#! models form#! models form
+class CommentForm(ModelForm):
+    # fields = "__all__"
+    class Meta:
+        model = Comment
+        fields = ('name', 'email', 'body')
+        # ? list = ModelName.objects.exclude(PassArguments)
+
+
+    
