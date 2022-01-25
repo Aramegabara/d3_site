ff48b0b02c5b486a2936c40ce541905ee7b2a186
aramegabara <geraskov94@gmail.com>
2022-01-25 20:12:47 +0100

    add tags

--- site_proj/blog_app/urls.py
+++ site_proj/blog_app/urls.py
@@ -5,9 +5,8 @@ app_name = 'blog_app_name'
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     #! ViewsList method
-    path('view/', views.PostListView.as_view(), name='post_list'),
+    # path('view/', views.PostListView.as_view(), name='post_list'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail'),
     path('<int:post_id>/share/', views.post_share, name='post_share'),
-    path('', views.post_list, name='post_list'),
     path('tag/<slug:tag__slug>/', views.post_list, name='post_tag'),
 ]
